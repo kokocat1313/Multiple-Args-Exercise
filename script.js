946d30ffc555cd6430a301cb2a214c1c10c595b6
kokocat1313 <66399733+kokocat1313@users.noreply.github.com>
2020-10-25 15:36:33 -0700

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+function isSnackEyes(num1, num2) {
+    if (num1 === 1 && num2 === 1){
+      return "Snack Eyes!"
+    }else{
+      return "Not Snack Eyes"
+    }
+    }
+    
+    console.log(isSnackEyes(1, 1))
